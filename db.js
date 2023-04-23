881c7d892ef3532e3d3770b6870c97a6646d20e1
JigarRajput <rajputjigar455@gmail.com>
2023-04-23 17:37:17 +0530

    mogodb connection changes

--- db.js
+++ db.js
@@ -1,3 +1,20 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/skdb", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/skdb", { useNewUrlParser: true });
+
+try {
+  //all the variables for mongodb connection
+  const url = process.env.MONGODB_URL;
+  const dbName = process.env.MONGODB_DATABASE;
+
+  //making the connection
+  mongoose.connect(url + dbName);
+  const con = mongoose.connection;
+
+  //checking the connection
+  con.on("open", () => {
+    console.log("Connection established with database");
+  });
+} catch {
+  console.log("Error Generated while making connection to the database");
+}
